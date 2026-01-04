2087e9375c4d3de1577538b37198605b55a78843
afsarzan <afsar_zan@yahoo.com>
2026-01-03 23:43:55 -0800

    feat: Update App component with introductory text and adjust Vite config plugin order

--- TTS_1/tailwind-nuances/src/App.tsx
+++ TTS_1/tailwind-nuances/src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <strong className='text-xl text:caret-amber-300 text-primary font-satoshi'>Tailwind</strong>
+      <p className='mt-4 text-secondary md:text-amber-400 text-4xl'>
+        This is a simple React application using Tailwind CSS with custom nuances like font from Fontshare and
+        customized colors.
+      </p>
     </>
   )
 }
